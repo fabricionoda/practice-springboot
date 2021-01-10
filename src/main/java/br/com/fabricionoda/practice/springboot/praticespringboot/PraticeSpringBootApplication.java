65f904d18e306183e5e0a7f05dfaceeb3a8c52a6
fabricio.noda <fabricio.noda@db1.com.br>
2021-01-10 15:05:06 -0300

    first

---
+++ src/main/java/br/com/fabricionoda/practice/springboot/praticespringboot/PraticeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabricionoda.practice.springboot.praticespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraticeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PraticeSpringBootApplication.class, args);
+	}
+
+}
